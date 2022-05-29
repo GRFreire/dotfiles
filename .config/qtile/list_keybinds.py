cf65e56b03d804a07ec66d564883912415af88b2
Guilherme Rugai Freire <dev@grfreire.com>
2022-05-29 16:26:34 -0300

    qtile: improve readability of keybinds window

--- .config/qtile/list_keybinds.py
+++ .config/qtile/list_keybinds.py
@@ -8,14 +8,14 @@ def columnate(matrix):
     def _columnate(matrix):
         widths = [max(map(len, map(str, col))) for col in zip(*matrix)]
         for row in matrix:
-            yield "  ".join((str(val).ljust(width) for val, width in zip(row, widths)))
+            yield "    |    ".join((str(val).ljust(width) for val, width in zip(row, widths)))
 
     return "\n".join(_columnate(matrix))
 
-matrix = [["Key", "Command"]]
+matrix = [["Key", "Command"], ["---", "-------"]]
 for key in c.keys:
     keys = ' + '.join((key.modifiers + [key.key]))
     desc = key.desc
     matrix.append([keys, desc])
 
-os.system(f"echo \"{columnate(matrix)}\" | col -bx | less")
+os.system(f"echo \"{columnate(matrix)}\" | col -bx | xargs -I \"[]\" printf \"\t%s\n\" \"[]\" | less")
