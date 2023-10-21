a568d8b728460bdfd196e503316b426ad12c54d4
Guilherme Rugai Freire <dev@grfreire.com>
2023-10-21 19:29:30 -0300

    fix xob audio indicator
    now arch linux requires pip installation of packages in separate
    envoirments, so I have to specify them when running the script

--- .config/xob/base.sh
+++ .config/xob/base.sh
@@ -2,4 +2,4 @@
 
 CONFIG="styles.cfg"
 
-([ "$1" = "-r" ] || [ "$1" = "--reload" ] && echo "$CONFIG" | entr -r "$0" "$2") || (python "$WATCHER" | xob -s "$1")
+([ "$1" = "-r" ] || [ "$1" = "--reload" ] && echo "$CONFIG" | entr -r "$0" "$2") || (.env/bin/python "$WATCHER" | xob -s "$1")
